b512fbc980f4fa5ca49cdbf2678a9aba8bbea2af
yunwhite <s2018s12@e-mirim.hs.kr>
2020-05-06 22:37:50 +0900

    #4 13-6 삼항 연산자 실습

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -22,6 +22,7 @@ namespace cscondition
                 Console.WriteLine("홀수 입니다!");
             }
             */
+            /*
             switch(input % 2)
             {
                 case 0:
@@ -31,6 +32,9 @@ namespace cscondition
                     Console.WriteLine("홀수 입니다!");
                     break;
             }
+            */
+            Console.WriteLine(input % 2 == 0 ? "짝수입니다" : "홀수입니다");
+            Console.WriteLine(input > 0 ? "자연수입니다" : "자연수가 아닙니다");
 
             // 현재 시간 구하기 
             Console.WriteLine(DateTime.Now.Year);
@@ -52,6 +56,36 @@ namespace cscondition
             else
             {
                 Console.WriteLine("저녁 먹을 시간입니다");   
+            }
+
+            Console.Write("이번 달은 몇 월인가요:");
+            int input2 = int.Parse(Console.ReadLine());
+            switch (input2)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("겨울입니다");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("봄입니다");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("여름입니다");
+                    break;
+
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("가을입니다");
+                    break;
+                default:
+                    Console.WriteLine("대체 어느 행성에 살고 계십니까?");
+                    break;
             }
         }
     }
