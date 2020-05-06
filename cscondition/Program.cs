fa023a293fa50ea49784c750e2fe7f917868d290
yunwhite <s2018s12@e-mirim.hs.kr>
2020-05-06 22:15:49 +0900

    13-2. if else 조건문

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -10,6 +10,43 @@ namespace cscondition
     {
         static void Main(string[] args)
         {
+            Console.Write("숫자 입력 :");
+            var input = int.Parse(Console.ReadLine());
+
+            if(input % 2 == 0)
+            {
+                Console.WriteLine("짝수 입니다!");
+            }else
+            {
+                Console.WriteLine("홀수 입니다!");
+            }
+            switch(input % 2)
+            {
+                case 0:
+                    Console.WriteLine("짝수 입니다!");                    
+                    break;
+                case 1:
+                    Console.WriteLine("홀수 입니다!");
+                    break;
+            }
+
+            // 현재 시간 구하기 
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+
+            // 오전과 오후 구분
+            if (DateTime.Now.Hour < 12)
+            {
+                Console.WriteLine("오전입니다");
+            }
+            else
+            {
+                Console.WriteLine("오후입니다");
+            }
         }
     }
 }
